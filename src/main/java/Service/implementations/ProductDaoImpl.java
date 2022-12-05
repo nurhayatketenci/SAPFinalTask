6095892545d9e14dd008c4728797a04db6dd56f0
nurhayatketenci <nurhayatktnn8@gmail.com>
2022-12-05 17:47:01 +0300

    commit

--- src/main/java/Service/implementations/ProductDaoImpl.java
+++ src/main/java/Service/implementations/ProductDaoImpl.java
@@ -15,7 +15,6 @@ public class ProductDaoImpl implements ProductDao {
         final Map<String, Object> params = new HashMap<String, Object>();
         String query =”SELECT {p:pk} FROM {Product AS p} WHERE {p:code} LIKE ?code”
         params.put("code",”1”);
-        //If do not want to pass any condition in query remove where clause and params as well but getting all products is not recommended
         final SearchResult<Product> searchResult = flexibleSearchService.search(query, params);
         return searchResult.getResults();
     }
